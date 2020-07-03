7a4e6ba309e0aa81c8123aa544d6b196175109b3
Raymundo Martinez <macbookpro@supermbdeofilms.home>
2020-07-03 14:17:22 -0700

    fixed update for signin

--- src/testingPlayground/testingPlayground.ts
+++ src/testingPlayground/testingPlayground.ts
@@ -7,6 +7,7 @@
 import CADL from '../'
 import { defaultConfig } from '../config'
 import DashboardMeetingroom from '../CADL/__mocks__/DashboardMeetingroom'
+import { Account } from '../services'
 
 import dot from 'dot-object'
 
@@ -22,38 +23,41 @@ export default (async function () {
     const cadl = new CADL({ ...defaultConfig })
     await cadl.init()
 
-    // await cadl.initPage('SignIn')
+    await cadl.initPage('SignIn')
+    // await cadl.initPage('CreateNewAccount')
     // debugger
-    // const vc = await Account.requestVerificationCode('+1 8858687687')
+    const vc = await Account.requestVerificationCode('+1 8858687687')
 
     // await cadl.root['CreateNewAccount'].formData.vertexAPI.store({
     //     confirmPassword: "letmein123",
     //     countryCode: "+1",
     //     password: "letmein123",
-    //     phoneNumber: "+1 8858687687",
+    //     phoneNumber: "+1 8858687dsdsd687",
     //     username: "username",
     //     verificationCode: vc
     // })
 
-    // await cadl.builtIn['signIn']({
-    //     password: "letmein123",
-    //     phoneNumber: "+1 8858687687",
-    //     verificationCode: vc
-    // })
-    // debugger
-    // cadl.root['SignIn'].update()
-    // debugger
-    // await cadl.initPage('DashboardMeetingroom')
+    await cadl.builtIn['signIn']({
+        password: "letmein123",
+        phoneNumber: "+1 8858687687",
+        verificationCode: vc
+    })
+    debugger
+    cadl.root['SignIn'].update()
+    // cadl.root['CreateNewAccount'].update()
+    debugger
+    await cadl.initPage('CreateMeeting')
+    debugger
     // await cadl.root['DashboardMeetingroom'].components[0].children[1].children[0].onClick[0].object()
     // debugger
-    await cadl.initPage('ApplyBusiness')
-    debugger
-    await cadl.root['ApplyBusiness'].formData.edgeAPI.store({ companyPhone: "+1 3431111dsdffsddsfd42essfsdfadfsdfsdsd1daf39" })
-    debugger
-    await cadl.root['ApplyBusiness'].formData.wciAPI.store({ type: 'text/plain', data: "+1 3009665sdsassaddsdsdsfsdfd1ffsdffsdfddaf39" })
-    debugger
-    await cadl.root['ApplyBusiness'].formData.w9API.store({ type: 'text/plain', data: "+1 hellow" })
-    debugger
+    // await cadl.initPage('ApplyBusiness')
+    // debugger
+    // await cadl.root['ApplyBusiness'].formData.edgeAPI.store({ companyPhone: "+1 3431111dsdffsddsfd42essfsdfadfsdfsdsd1daf39" })
+    // debugger
+    // await cadl.root['ApplyBusiness'].formData.wciAPI.store({ type: 'text/plain', data: "+1 3009665sdsassaddsdsdsfsdfd1ffsdffsdfddaf39" })
+    // debugger
+    // await cadl.root['ApplyBusiness'].formData.w9API.store({ type: 'text/plain', data: "+1 hellow" })
+    // debugger
 
 
     // //@ts-ignore
