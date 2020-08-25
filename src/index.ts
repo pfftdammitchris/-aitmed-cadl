8552d3cf3cf1512996712c238ed88d40d17c472a
Raymundo Martinez <macbookpro@SUPERMBDEOFILMS.lan>
2020-08-25 11:27:17 -0700

    WIP document upload

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import CADL from './CADL'
 
-import { Account, Note, Notebook } from './services'
+import { Account, Note, Notebook, Document } from './services'
 
-export { Account, Note, Notebook }
+export { Account, Note, Notebook, Document }
 export * from './CADL'
 export default CADL
