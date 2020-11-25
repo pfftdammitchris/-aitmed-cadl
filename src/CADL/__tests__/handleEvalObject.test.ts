a3397f4aba6f0fd61844c34b8fc6f1d6a1c2787b
Raymundo Martinez <macbookpro@Clint-Eastwood.lan>
2020-11-25 15:39:18 -0800

    testing evalobject

---
+++ src/CADL/__tests__/handleEvalObject.test.ts
@@ -0,0 +1,7 @@
+import CADL from '../CADL'
+import { defaultConfig } from '../../config'
+import TestPage from '../__mocks__/TestPage'
+
+describe('should evaluate object commands', () => {
+  it('should evaluate assignment', () => {})
+})
