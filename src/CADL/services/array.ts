9dc607e205e878f7e7178b806fa6f5d4538c4e0c
JonathanChen <jonathanc@aitmed.com>
2020-11-27 14:38:11 -0800

    add clear and removebykey methods

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -1,14 +1,31 @@
 import _, { isArray } from 'lodash'
 
 export default {
-  add({ object, value }) {
-    if (isArray(object)) {
-      if (value) {
-        var cloned = _.cloneDeep(value)
-        object.push(cloned)
-      }
-      return
-    }
-    return
-  },
+    add({ object, value }) {
+        if (isArray(object)) {
+            if (value) {
+                var cloned = _.cloneDeep(value)
+                object.push(cloned)
+            }
+            return
+        }
+        return
+    },
+    clear({ object }) {
+        if (isArray(object)) {
+            object.length = 0
+        }
+        return
+    },
+    removeByKey({ object, key }) {
+        if (isArray(object)) {
+            for (let i = 0; i < object.length; i++) {
+                if (object[i][key]) {
+                    object.splice(i, 1)
+                    return
+                }
+            }
+        }
+        return
+    },
 }
