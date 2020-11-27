eade831e00476edcc3bf4bfb0ad8629ecb9a74f4
JonathanChen <jonathanc@aitmed.com>
2020-11-27 14:39:44 -0800

    add comments for removeByKey

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -21,6 +21,8 @@ export default {
         if (isArray(object)) {
             for (let i = 0; i < object.length; i++) {
                 if (object[i][key]) {
+                    // TO DO: how to handle objects with same key? should they all be deleted, or just delete first one?
+                    // Should duplicate object key made not allowed using add method?
                     object.splice(i, 1)
                     return
                 }
