8b97e257f38949fb6de21e373c94f84e819e5fba
JonathanChen <jonathanc@aitmed.com>
2020-12-04 11:06:34 -0800

    add builtIn.array has, push, append methods, tested has method

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -11,9 +11,39 @@ export default {
         }
         return
     },
+    append({ newMessage, messages }) {
+        if (isArray(messages)) {
+            if (newMessage) {
+                var cloned = _.cloneDeep(newMessage)
+                messages.push(cloned)
+            }
+            return
+        }
+        return
+    },
     clear({ object }) {
         if (isArray(object)) {
             object.length = 0
+        }
+        return
+    },
+    has({ object, key }) {
+        if (isArray(object)) {
+            for (let i = 0; i < object.length; i++) {
+                if (object[i].key === key) {
+                    return true
+                }
+            }
+        }
+        return false
+    },
+    push({ newMessage, messages }) {
+        if (isArray(messages)) {
+            if (newMessage) {
+                var cloned = _.cloneDeep(newMessage)
+                messages.unshift(cloned)
+                return
+            }
         }
         return
     },
@@ -31,3 +61,5 @@ export default {
         return
     },
 }
+
+
