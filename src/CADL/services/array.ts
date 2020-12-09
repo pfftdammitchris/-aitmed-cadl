a2bb91065aabd20c00573de75a1b79b5cb3d9ff5
Raymundo Martinez <macbookpro@Clint-Eastwood.local>
2020-12-08 16:52:28 -0800

    merged with master

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -27,56 +27,38 @@ export default {
           return
         }
         return
-    },
-    append({ newMessage, messages }) {
-        if (isArray(messages)) {
-            if (newMessage) {
-                var cloned = _.cloneDeep(newMessage)
-                messages.push(cloned)
-            }
-            return
+      }
+    }
+  },
+  append({ newMessage, messages }) {
+    if (isArray(messages)) {
+      if (newMessage) {
+        var cloned = _.cloneDeep(newMessage)
+        messages.push(cloned)
+      }
+      return
+    }
+    return
+  },
+
+  has({ object, key }) {
+    if (isArray(object)) {
+      for (let i = 0; i < object.length; i++) {
+        if (object[i].key === key) {
+          return true
         }
+      }
+    }
+    return false
+  },
+  push({ newMessage, messages }) {
+    if (isArray(messages)) {
+      if (newMessage) {
+        var cloned = _.cloneDeep(newMessage)
+        messages.unshift(cloned)
         return
-    },
-    clear({ object }) {
-        if (isArray(object)) {
-            object.length = 0
-        }
-        return
-    },
-    has({ object, key }) {
-        if (isArray(object)) {
-            for (let i = 0; i < object.length; i++) {
-                if (object[i].key === key) {
-                    return true
-                }
-            }
-        }
-        return false
-    },
-    push({ newMessage, messages }) {
-        if (isArray(messages)) {
-            if (newMessage) {
-                var cloned = _.cloneDeep(newMessage)
-                messages.unshift(cloned)
-                return
-            }
-        }
-        return
-    },
-    removeByKey({ object, key }) {
-        if (isArray(object)) {
-            for (let i = 0; i < object.length; i++) {
-                if (object[i][key]) {
-                    // TO DO: how to handle objects with same key? should they all be deleted, or just delete first one?
-                    // Should duplicate object key made not allowed using add method?
-                    object.splice(i, 1)
-                    return
-                }
-            }
-        }
-        return
-    },
+      }
+    }
+    return
+  },
 }
-
-
