7a2eccc59e4308ef6ff8e3f1290c9978954ac5eb
Raymundo Martinez <macbookpro@Clint-Eastwood.lan>
2020-12-28 14:25:36 -0800

    wip

--- src/CADL/CADL.ts
+++ src/CADL/CADL.ts
@@ -1985,18 +1985,16 @@ export default class CADL extends EventEmitter {
           const { pageName, dataKey, value, replace } = action.payload
           let currVal
           let newVal = value
-          //used to merge new value to existing value ref
-          if (typeof pageName === 'undefined') {
-            currVal = _.get(state, dataKey)
-          } else {
-            currVal = _.get(state[pageName], dataKey)
+          if (!replace) {
+            //used to merge new value to existing value ref
+            if (typeof pageName === 'undefined') {
+              currVal = _.get(state, dataKey)
+            } else {
+              currVal = _.get(state[pageName], dataKey)
+            }
           }
           if (isObject(currVal) && isObject(newVal)) {
-            if (replace) {
-              newVal = _.merge(newVal, currVal)
-            } else {
-              newVal = _.merge(currVal, newVal)
-            }
+            newVal = _.merge(currVal, newVal)
           } else if (Array.isArray(currVal) && Array.isArray(newVal)) {
             currVal.length = 0
             currVal.push(...newVal)
