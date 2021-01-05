03f046ba7fc6ef65ccfe4e5164dad4d8d67c2f23
Chenchen XU <chenchen.xu@aitmed.com>
2021-01-05 20:21:18 +0800

    update

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -75,34 +75,7 @@ export default {
     }
     return
   },
-  LoopToGenerate({ span }) {
-    let fotmat = (n: number) => {
-      if (n < 13 * 60) {
-        let h = Math.floor(n / 60)
-        let m = n % 60
-        return `${`0${h}`.slice(-2)}:${`0${m}`.slice(-2)}`
-      } else {
-        let h = Math.floor((n - 12 * 60) / 60)
-        let m = (n - 12 * 60) % 60
-        return `${`0${h}`.slice(-2)}:${`0${m}`.slice(-2)}pm`
-      }
-    }
-    let i: number = 0
-    let arr: any[] = []
-    while (i <= 24 * 60) {
-      arr.push(fotmat(i))
-      i += parseInt(span)
-    }
-    console.log(arr)
-    return arr
-  },
-  // get the length of object
-  getListLength({ object }) {
-    if (isArray(object)) {
-      return object.length
-    }
-    return 0
-  },
+
   // convert ["anemia", "anxiety", "arthritis"] to [{key: "anemia"},{key: "anxiety"},{key: "arthritis"}] for listObject
   covertToJsonArray({ array }) {
     let dataObject: Record<string, any> = []
@@ -115,37 +88,11 @@ export default {
     }
     return `${array}is not an array`
   },
-  calendarArray({ year, month }) {
-    year = parseInt(year)
-    month = parseInt(month)
-    let dataObject: Record<string, any> = []
-    let isLeapYear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? true : false
-    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    days[1] = isLeapYear ? 29 : 28
-    let dataArray = new Array(42).fill(0)
-    // What day is the first day of the month
-    let day = new Date(`${year}-${month}-1`).getDay();
-    for (let i = day, j = 1; i < day + days[month - 1]; i++, j++) {
-      dataArray[i] = j
+  // get the length of object
+  getListLength({ object }) {
+    if (isArray(object)) {
+      return object.length
     }
-    // let frontMonth = month == 1 ? days[11] : days[month - 2]
-    //  fill other day
-    for (let i = day - 1, j = 0; i >= 0; i--, j++) {
-      if (day == 0) break
-      else {
-        // dataArray[i] = frontMonth - j
-        dataArray[i] = ''
-      }
-    }
-    for (let i = day + days[month - 1], j = 1; i < 42; i++, j++) {
-      // dataArray[i] = j
-      dataArray[i] = ''
-    }
-    //  convert dataarray to jsononject
-    for (let i = 0; i < dataArray.length; i++) {
-      dataObject.push({ key: dataArray[i] })
-    }
-    // let dataJson = _.chunk(dataArray, 7);
-    return dataObject
-  }
+    return 0
+  },
 }
--- src/CADL/services/date.ts
+++ src/CADL/services/date.ts
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+import _, { isArray } from 'lodash'
 export default {
     getDate() {
         return new Date().getDate()
@@ -13,13 +13,15 @@ export default {
         return new Date().getTimezoneOffset()
     },
     /**
-     * Return the number of milliseconds between January 1, 1970
+     * return time stamp (s)
      */
     getTime() {
-        return new Date().getTime()
+        let date = new Date().toString()
+        let stamp = Date.parse(date) / 1000
+        return stamp
     },
     /**
-     * Returns the time stamp interval of a day  (ms)
+     * Returns the time stamp interval of a day  (s)
      * @param {string} (date) -->  MM-DD-YY
      * @return 
      */
@@ -29,8 +31,71 @@ export default {
             start: 0,
             end: 0
         }
-        timeStamp.start = Date.parse(date)
-        timeStamp.end = timeStamp.start + 86400000
+        let dateArray = date.split('-')
+        dateArray[0] = parseInt(dateArray[0]) - 1
+        let dateObject = new Date()
+        dateObject.setMonth(dateArray[0])
+        dateObject.setDate(dateArray[1])
+        dateObject.setFullYear(dateArray[2])
+        dateObject.setHours(0)
+        dateObject.setMinutes(0)
+        dateObject.setSeconds(0)
+        timeStamp.start = Date.parse(dateObject.toString()) / 1000
+        timeStamp.end = timeStamp.start + 86400
         return timeStamp
+    },
+    LoopToGenerate({ span }) {
+        let fotmat = (n: number) => {
+            if (n < 13 * 60) {
+                let h = Math.floor(n / 60)
+                let m = n % 60
+                return `${`0${h}`.slice(-2)}:${`0${m}`.slice(-2)}am`
+            } else {
+                let h = Math.floor((n - 12 * 60) / 60)
+                let m = (n - 12 * 60) % 60
+                return `${`0${h}`.slice(-2)}:${`0${m}`.slice(-2)}pm`
+            }
+        }
+        let i: number = 0
+        let arr: any[] = []
+        while (i <= 24 * 60) {
+            arr.push(fotmat(i))
+            i += parseInt(span)
+        }
+        console.log(arr)
+        return arr
+    },
+    calendarArray({ year, month }) {
+        year = parseInt(year)
+        month = parseInt(month)
+        let dataObject: Record<string, any> = []
+        let isLeapYear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? true : false
+        let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        days[1] = isLeapYear ? 29 : 28
+        let dataArray = new Array(42).fill(0)
+        // What day is the first day of the month
+        let day = new Date(`${year}-${month}-1`).getDay();
+        for (let i = day, j = 1; i < day + days[month - 1]; i++, j++) {
+            dataArray[i] = j
+        }
+        // let frontMonth = month == 1 ? days[11] : days[month - 2]
+        //  fill other day
+        for (let i = day - 1, j = 0; i >= 0; i--, j++) {
+            if (day == 0) break
+            else {
+                // dataArray[i] = frontMonth - j
+                dataArray[i] = ''
+            }
+        }
+        for (let i = day + days[month - 1], j = 1; i < 42; i++, j++) {
+            // dataArray[i] = j
+            dataArray[i] = ''
+        }
+        //  convert dataarray to jsononject
+        for (let i = 0; i < dataArray.length; i++) {
+            dataObject.push({ key: dataArray[i], color: '0xf5b4ae', backgroundColor: "#000000" })
+        }
+        // let dataJson = _.chunk(dataArray, 7);
+        return dataObject
     }
 }
