82c4ef966c025e9c4e830e3bc8a5df2367afde50
Raymundo Martinez <macbookpro@Clint-Eastwood.lan>
2021-01-11 14:01:24 -0800

    added prepareDoc fn

--- src/CADL/services/utils.ts
+++ src/CADL/services/utils.ts
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import store from '../../common/store'
 import { isPopulated } from '../utils'
+import { documentToNote } from '../../services/document/utils'
 
 export default {
   base64ToBlob({
@@ -24,5 +25,20 @@ export default {
       }
     }
     return true
+  },
+
+  async prepareDoc({ doc }: { doc: Record<string, any> }) {
+    const note = await documentToNote({ document: doc })
+    const { name } = note
+    if (!name?.data) return
+    if (typeof name?.data !== 'string') return
+    if (!isPopulated(name?.data)) return
+    const blob = store.level2SDK.utilServices.base64ToBlob(
+      name?.data,
+      name?.type
+    )
+    const blobUrl = URL.createObjectURL(blob)
+    name.data = blobUrl
+    return note
   },
 }
