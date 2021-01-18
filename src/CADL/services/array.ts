a957dd7c1f4651fb4049574585f494fc00add31d
Chenchen XU <chenchen.xu@aitmed.com>
2021-01-18 17:00:35 +0800

    update delete

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -54,6 +54,23 @@ export default {
     }
     return
   },
+  removeByName({ object, key, name }) {
+    //the format of the array must be [ man: man]  man： man
+    if (isArray(object)) {
+      for (let i = 0; i < object.length; i++) {
+        if (object[i][key] == name || object[i][key] == "") {
+          // TO DO: how to handle objects with same key? should they all be deleted, or just delete first one?
+          // Should duplicate object key made not allowed using add method?
+          object.splice(i, 1)
+          console.log(i);
+          return
+        } else {
+          console.log('false', 'color: red');
+        }
+      }
+    }
+    return
+  },
   removeByValue({ object, value }) {
     if (isArray(object)) {
       for (let i = 0; i < object.length; i++) {
@@ -67,6 +84,9 @@ export default {
     }
     return
   },
+
+
+
   append({ newMessage, messages }) {
     if (isArray(messages)) {
       if (newMessage) {
