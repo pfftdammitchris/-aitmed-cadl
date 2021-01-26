ea31d1149682c222cf74c591cb312d74f5b38de9
Raymundo Martinez <macbookpro@Clint-Eastwood.lan>
2021-01-25 16:43:03 -0800

    added formatTimer fn

--- src/CADL/services/string.ts
+++ src/CADL/services/string.ts
@@ -2,6 +2,9 @@ import moment from 'moment'
 import humanizeDuration from 'humanize-duration'
 
 export default {
+  formatTimer(unixTime: number) {
+    return moment(unixTime * 1000).format('HH:mm:ss')
+  },
   formatUnixtime_en(unixTime: number) {
     return moment(unixTime * 1000).format('lll')
   },
