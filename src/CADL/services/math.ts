a6ad43ea27c33b322683cb9fcb9477d9993130da
Yuhan Gao <yhgaoblair@gmail.com>
2021-03-12 15:49:58 -0800

    math update

--- src/CADL/services/math.ts
+++ src/CADL/services/math.ts
@@ -3,7 +3,7 @@ export default {
     const rand = Math.random()
     return rand
   },
-  add: function ({ num1, num2 }): number{
+  add: function ({ num1, num2 }): number {
     return num1 + num2
   },
   greater: function ({ num1, num2 }): boolean {
