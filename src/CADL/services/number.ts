95a655f982189c9616aaa4c222f428c599fb396c
SuperGodK <836676738@qq.com>
2021-03-25 17:54:25 +0800

    add function

--- src/CADL/services/number.ts
+++ src/CADL/services/number.ts
@@ -19,21 +19,33 @@ export default {
     }
     return 0
   },
-  getAuthority({ binary }) {
-    class Authority {
-      create: boolean
-      edit: boolean
-      review: boolean
-    }
-    let authority: string[] = []
-    let medicalFacilityInfo = new Authority()
-    // let authorityList = new Authority()
-    if (binary) {
-      authority = binary.split("")
-    }
-    if (authority[0] === "1")
-      if (authority[1] === "1")
-        // authorityList.medicalFacilityInfo.create = true
-        return authorityList
+  getAuthority({ authSubtype, authList }) {
+    let authDoc: any = {}
+    authSubtype.forEach((item: number) => {
+      /***
+       * convert subtype to Hex , get the authority type ,and the  authority level
+       * authority level： '1 1 1' --> 'invite edit review'
+       *  */
+      let authHex: number = parseInt(item.toString(16)) // convert decimal to hexadecimal 
+      let authType: number = parseInt((authHex / 10000).toString()) // get authority type from High Bit , and authority level low bit
+      let authLevel: number = authHex % 10
+      let authName: string = ""
+      // convert authority level to object
+      let authBinary = authLevel.toString(2).split('');
+      let resArray = authBinary.map((item) => {
+        return (item === '1') ? true : false
+      })
+      Object.keys(authList).forEach((key: any) => {
+        if (authList[key] == authType) {
+          authName = key
+        }
+      })
+      authDoc[authName] = {
+        invite: resArray[0],
+        edit: resArray[1],
+        review: resArray[2]
+      }
+    })
+    return authDoc
   }
 }
--- src/CADL/services/object.ts
+++ src/CADL/services/object.ts
@@ -49,32 +49,49 @@ export default {
   extract({ array, field }) {
     let match: string[] = field.split(".")
     let result: string[] = []
+    // let str: string = "array"
+    // match.forEach(arr => {
+    //   str += "[\'" + arr + "\']"
+    // })
     if (isArray(array)) {
       if (match.length === 1) {
-        array.forEach(arr => {
-          let str = arr[`${match[0]}${match[1]}`]
-          alert(str)
-          result.push(str)
+        array.forEach(array => {
+          result.push(array[match[0]])
         });
       }
       else if (match.length === 2) {
         array.forEach(arr => {
-
           result.push(arr[match[0]][match[1]])
         });
       }
     }
     else if (array) {
       if (match.length === 1) {
-        let str = array[match[0]][match[1]]
-        alert(typeof (str))
-        result.push(str)
+        result.push(array[match[0]])
       }
       else if (match.length === 2) {
-        let str = array[`${match[0]}${match[1]}`]
-        result.push(str)
+        result.push(array[match[0]][match[1]])
       }
     }
+    return result
+  },
+  authToSubType({ auth, authList }) {
+    let result: number[] = []
+    Object.keys(auth).forEach((key: any) => {
+      let authType = 0
+      Object.keys(authList).forEach((arr) => {
+        if (key === arr) {
+          authType = authList[arr] * 10000
+        }
+      })
+      if (auth[key]['edit'] === true)
+        authType += 4
+      if (auth[key]['invite'] === true)
+        authType += 2
+      if (auth[key]['review'] === true)
+        authType += 1
+      result.push(parseInt(authType.toString(), 16))
+    })
     return result
   }
 }
