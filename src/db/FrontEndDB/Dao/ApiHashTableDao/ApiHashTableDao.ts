16d6979beb8c2a1d52df968bf805896e9369b7e2
Raymundo Martinez <macbookpro@Clint-Eastwood.lan>
2021-05-19 09:42:57 -0700

    wip

---
+++ src/db/FrontEndDB/Dao/ApiHashTableDao/ApiHashTableDao.ts
@@ -0,0 +1,37 @@
+export default (db) => {
+  return {
+    getApiResult,
+    insertApiResult,
+    deleteApiResult,
+  }
+  function getApiResult(apiInputHash) {
+    const sqlstr =
+      'SELECT resultId FROM api_hash_table WHERE api_input_hash = :api_input_hash'
+    const params = {
+      ':api_input_hash': apiInputHash,
+    }
+    const res = db.exec(sqlstr, params)
+    return res[0].values[0][0]
+  }
+  function insertApiResult(apiInputHash, apiResult) {
+    const sqlstr =
+      'INSERT INTO api_hash_table VALUES (:api_input_hash, :api_result );'
+    const params = {
+      ':api_input_hash': apiInputHash,
+      ':api_result': JSON.stringify(apiResult),
+    }
+
+    const res = db.exec(sqlstr, params)
+    return res
+  }
+
+  function deleteApiResult(apiInputHash) {
+    const sqlstr =
+      'DELETE FROM api_hash_table WHERE api_input_hash = :api_input_hash'
+    const params = {
+      ':api_input_hash': apiInputHash,
+    }
+    const res = db.exec(sqlstr, params)
+    return res[0].values[0][0]
+  }
+}
