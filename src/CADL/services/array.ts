86a73a11afd1294776de3a8adad45060142bdfe3
RZ <zhangrui2804@163.com>
2021-06-08 20:16:25 +0800

    update

--- src/CADL/services/array.ts
+++ src/CADL/services/array.ts
@@ -827,15 +827,5 @@ export default {
     if (it >= 7) return true
     else return false
   },
-  selectOneToArr({ arr, key }) {
-    let arr1: Array<any> = new Array
-    for (let i = 0; i < arr.length; i++)
-      arr1.push(arr[i][key])
-    return arr1
-  },
-  matchInArray({ arr, value, key, key1 }) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i][key] == value) return arr[i][key1]
-    }
-  },
+
 }
