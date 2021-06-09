d11d00fa198b3891f48e160b8cfa06e39d0d0205
YaoKai <kai.yao@aitmed.com>
2021-06-09 19:17:50 +0800

    添加less函数

--- src/CADL/services/number.ts
+++ src/CADL/services/number.ts
@@ -63,6 +63,11 @@ export default {
     num = parseInt(num)
     step = parseInt(step)
     return (num - step)
+  },
+  less({ num1, num2 }) {
+    if (num1 < num2)
+      return true
+    else
+      return false
   }
-
 }
