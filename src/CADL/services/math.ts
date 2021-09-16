9412e9dc9ff4ab7dd2dafe3031baf60047da92be
jianbo.feng <jianbo.feng@aitmed.com>
2021-09-16 14:14:41 +0800

    增加lessthanSize校验上传图片大小

--- src/CADL/services/math.ts
+++ src/CADL/services/math.ts
@@ -8,5 +8,13 @@ export default {
   },
   greater: function ({ num1, num2 }): boolean {
     return num1 > num2
+  },
+
+  lessthanSize: function({object,key}): boolean {
+　　if (parseFloat(object).toString() !== "NaN") { 
+     let maxSize: number = Math.floor(key*Math.pow(1024,2))
+        return  object > maxSize ? false : true        
+  　　} 
+    return false
   }
 }
