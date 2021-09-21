754b6e623e70babc6a1b277c481f66700d087206
christopher@aitmed.com <pfftdammitchris@gmail.com>
2021-09-21 10:43:50 -0700

    1.0.382

--- src/utils/yaml.ts
+++ src/utils/yaml.ts
@@ -1,4 +1,11 @@
 import YAML from 'yaml'
+import fs from 'fs-extra'
+import path from 'path'
+
+const yml = fs.readFileSync(path.join(process.cwd(), 'yml.yml'), 'utf8')
+console.log(yml)
+const obj = parseYml(yml)
+console.log(obj)
 
 export function parseYml(yml = ''): Record<string, any> {
   return YAML.parse(yml, {
