d467619ed153170f850bde99b33f14cee2914300
christopher@aitmed.com <pfftdammitchris@gmail.com>
2021-09-21 11:01:40 -0700

    G

--- src/utils/yaml.ts
+++ src/utils/yaml.ts
@@ -1,17 +1,23 @@
 import YAML from 'yaml'
-import fs from 'fs-extra'
-import path from 'path'
 
-const yml = fs.readFileSync(path.join(process.cwd(), 'yml.yml'), 'utf8')
-console.log(yml)
-const obj = parseYml(yml)
-console.log(obj)
+// import fs from 'fs-extra'
+// import path from 'path'
+
+// const yml = fs.readFileSync(path.join(process.cwd(), 'yml.yml'), 'utf8')
+// console.log(yml)
+// const obj = parseYml(yml)
+// console.log(obj)
 
 export function parseYml(yml = ''): Record<string, any> {
-  return YAML.parse(yml, {
-    merge: true,
-    prettyErrors: true,
-    schema: 'core',
-    version: '1.2',
-  })
+  try {
+    return YAML.parse(yml, {
+      logLevel: 'debug',
+      prettyErrors: true,
+      schema: 'failsafe',
+      version: '1.2',
+    })
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
 }
